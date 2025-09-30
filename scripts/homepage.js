5b99223ef7bb58bb8d52f3736052aa6657042210
Naomi <naomimcborn@gmail.com>
2025-09-30 21:16:49 +0200

    Add settings button

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -1,5 +1,32 @@
 document.getElementById("startBtn").addEventListener('click', startButtonClicked);
-
 function startButtonClicked() {
-    alert("Not yet implemented");
+    var selectedTheme = document.getElementById("themes").value;
+    var pageLink = "";
+
+    switch (selectedTheme) {
+        case "studyLofi":
+            break;
+        case "FFV":
+            break;
+    }
+    if (selectedTheme == "studyLofi") {
+        alert("studyLofi selected")
+    }
+    if (selectedTheme == "FFV") {
+        alert("FFV selected")
+    }
+    if (selectedTheme == "FFVI") {
+        alert("FFVI selected")
+    }
+    if (selectedTheme == "FFVII") {
+        // alert("FFVII selected")
+        pageLink = "../pages/FFVIIPage.html"
+        window.location.href(pageLink);
+    }
+    if (selectedTheme == "FFXV") {
+        alert("FFXV selected")
+    }
+    if (selectedTheme == "folder") {
+        alert("folder selected")
+    }
 }
