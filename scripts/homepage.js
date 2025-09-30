3dfd25eb1a73a529c613733afaa32c05624601da
Naomi <naomimcborn@gmail.com>
2025-09-30 22:55:56 +0200

    Changes in progress

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -1,32 +1,22 @@
 document.getElementById("startBtn").addEventListener('click', startButtonClicked);
+
 function startButtonClicked() {
     var selectedTheme = document.getElementById("themes").value;
-    var pageLink = "";
 
     switch (selectedTheme) {
         case "studyLofi":
             break;
-        case "FFV":
+        case "FFVII":
             break;
-    }
-    if (selectedTheme == "studyLofi") {
-        alert("studyLofi selected")
-    }
-    if (selectedTheme == "FFV") {
-        alert("FFV selected")
-    }
-    if (selectedTheme == "FFVI") {
-        alert("FFVI selected")
-    }
-    if (selectedTheme == "FFVII") {
-        // alert("FFVII selected")
-        pageLink = "../pages/FFVIIPage.html"
-        window.location.href = pageLink;
-    }
-    if (selectedTheme == "FFXV") {
-        alert("FFXV selected")
-    }
-    if (selectedTheme == "folder") {
-        alert("folder selected")
+        case "FFVII":
+            window.location.href = "../MusicPlayerWidget/pages/FFVIIPage.html";
+            break;
+        case "FFXV":
+            break;
+        case "folder":
+            break;
+        default:
+            window.location.href = "../MusicPlayerWidget/index.html";
+
     }
 }
