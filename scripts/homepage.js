b3baf3132239e31a918a14210a20d6ab1ea35aab
Naomi <57104986+nmcb02@users.noreply.github.com>
2025-10-22 14:40:00 +0200

    Feature/#7 add buttons plus functions (#10)
    * Let button show a alert
    
    * Add settings button
    
    * Makke one option go to another page
    
    * Changes in progress
    
    * Update readme with windows commands
    
    * Fix switch case not working
    
    * Create a basic music player page without functionality
    
    * Add a lofi page
    
    * Change page name in index.html

---
+++ scripts/homepage.js
@@ -0,0 +1,18 @@
+document.getElementById("startBtn").addEventListener('click', startButtonClicked);
+
+function startButtonClicked() {
+    var selectedTheme = document.getElementById("themes").value.trim();
+
+    switch (selectedTheme) {
+        case "Amethyst":
+            window.location.href = "../MusicPlayerWidget/pages/studyLofi.html";
+            break;
+        case "FFVII":
+            window.location.href = "../MusicPlayerWidget/pages/FFVIIPage.html";
+            break;
+        case "folder":
+            break;
+        default:
+            window.location.href = "../MusicPlayerWidget/index.html";
+    }
+}
