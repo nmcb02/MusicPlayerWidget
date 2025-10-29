64b9e2b245a9325b375565463592d7b2656d7348
Naomi <naomimcborn@gmail.com>
2025-10-29 16:57:45 +0100

    Add a working progressbar and buttons for music ctrls play, pause, begin, end

---
+++ scripts/musicPlayerControls.js
@@ -0,0 +1,38 @@
+let progress = document.getElementById('progressBar');
+let song = document.getElementById('song');
+let playButton = document.getElementById('playSong');
+
+song.onloadstart = function () {
+    progress.max = song.duration;
+    progress.value = song.currentTime;
+}
+
+function playPause() {
+    if (song.paused) {
+        song.play();
+        playButton.innerHTML = '⏸';
+    } else {
+        song.pause();
+        playButton.innerHTML = '▶';
+    }
+}
+
+function previousSong() {
+    song.currentTime = 0;
+}
+
+function nextSong() {
+    song.currentTime = song.duration;
+}
+
+if (song.play) {
+    setInterval(() => {
+        progress.value = song.currentTime;
+    }, 500);
+}
+
+progress.onchange = function () {
+    song.play;
+    song.currentTime = progress.value;
+    playButton.innerHTML = '⏸';
+}
