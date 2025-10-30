271e44764498cd25bb7c8e5aa1a04da0b0cc15c6
Naomi <naomimcborn@gmail.com>
2025-10-30 22:15:53 +0100

    Add song title name

--- scripts/musicPlayerControls.js
+++ scripts/musicPlayerControls.js
@@ -3,6 +3,8 @@ let song = document.getElementById('song');
 let playButton = document.getElementById('playSong');
 let totalTime = document.getElementById('totalTime');
 let currentTimeEl = document.getElementById('currentTime');
+let songName = document.getElementById('songName');
+let artistName = document.getElementById('artistName');
 
 function formatTime(sec) {
     const minutes = Math.floor(sec / 60);
@@ -14,6 +16,12 @@ song.addEventListener('loadedmetadata', () => {
     progress.max = song.duration; // duration in seconds
     progress.value = song.currentTime;
     totalTime.textContent = formatTime(song.duration);
+
+    // Extract filename without extension
+    let rawName = song.currentSrc.split('/').pop().split('.').slice(0, -1).join('.');
+    songName.textContent = decodeURIComponent(rawName);
+
+    artistName.textContent = "Square Enix"; // Static for now, could be dynamic later
 });
 
 // update both slider + current time as song plays
